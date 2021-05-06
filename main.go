c2326454467f38bde63887c41878a533f4fee580
skrevolve <sukyu0919@naver.com>
2021-05-06 09:34:45 +0900

    modify mydict

--- main.go
+++ main.go
@@ -28,6 +28,10 @@ import (
 
 func main() {
 	dictionary := mydict.Dictionary{"first": "firstword"}
-
-	fmt.Println(dictionary)
+	definition, err := dictionary.Search("first")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(definition)
+	}
 }
