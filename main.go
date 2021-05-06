05f0bd4847ea4c32f50364e65feaa715e8a99c03
skrevolve <sukyu0919@naver.com>
2021-05-06 12:38:31 +0900

    modify checkURL

--- main.go
+++ main.go
@@ -18,41 +18,89 @@
 // }
 //----------------------------------------------
 
+// package main
+
+// import (
+// 	"fmt"
+
+// 	"github.com/seokyu/learngo/mydict"
+// )
+
+// func main() {
+// 	dictionary := mydict.Dictionary{}
+// 	baseWord := "hello"
+// 	// definition := "Greeting"
+// 	// err := dictionary.Add(word, definition)
+// 	// if err != nil {
+// 	// 	fmt.Println(err)
+// 	// }
+// 	// hello, _ := dictionary.Search(word)
+// 	// fmt.Println(hello)
+
+// 	// err2 := dictionary.Add(word, definition)
+// 	// if err2 != nil {
+// 	// 	fmt.Println(err2)
+// 	// }
+// 	dictionary.Add(baseWord, "first")
+// 	// err := dictionary.Update("asdfasdf", "second")
+// 	// if err != nil {
+// 	// 	fmt.Println(err)
+// 	// }
+// 	dictionary.Search(baseWord)
+// 	dictionary.Delete(baseWord)
+// 	word, err := dictionary.Search(baseWord)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	} else {
+// 		fmt.Println(word)
+// 	}
+// 	fmt.Println(word)
+// }
+// --------------------------------------------
+
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/seokyu/learngo/mydict"
+	"net/http"
 )
 
-func main() {
-	dictionary := mydict.Dictionary{}
-	baseWord := "hello"
-	// definition := "Greeting"
-	// err := dictionary.Add(word, definition)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// hello, _ := dictionary.Search(word)
-	// fmt.Println(hello)
+var errorRequestFailed = errors.New("Request failed")
 
-	// err2 := dictionary.Add(word, definition)
-	// if err2 != nil {
-	// 	fmt.Println(err2)
-	// }
-	dictionary.Add(baseWord, "first")
-	// err := dictionary.Update("asdfasdf", "second")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	dictionary.Search(baseWord)
-	dictionary.Delete(baseWord)
-	word, err := dictionary.Search(baseWord)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(word)
+type result struct {
+	url    string
+	status string
+}
+
+func main() {
+	//results := make(map[string]string)
+	c := make(chan result)
+	urls := []string{
+		"https://www.airbnb.com",
+		"https://www.google.com",
+		"https://www.amazon.com",
+		"https://www.reddit.com",
+		"https://www.facebook.com",
+		"https://www.instagram.com",
+		"https://soundcloud.com",
 	}
-	fmt.Println(word)
+
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		fmt.Println(<-c)
+	}
+}
+
+func hitURL(url string, c chan<- result) {
+	fmt.Println("Checking: ", url)
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
+	}
+	c <- result{url: url, status: status}
 }
