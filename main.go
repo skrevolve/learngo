e38b0dd6c7eb04bf392984328472f42b66f5b2a1
skrevolve <sukyu0919@naver.com>
2021-05-06 15:51:42 +0900

    udate

--- main.go
+++ main.go
@@ -113,24 +113,39 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
-	"github.com/PuerkitoBio/goquery"
+	"github.com/PuerkitoBio/goquery" //you have to download in $GOPATH(/src/github.com/)
 )
 
-var baseURL = "https://kr.indeed.com/jobs?q=python&limit=50"
+var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
-	pages := getPages()
+	totalPages := getPages()
+	for i := 0; i < totalPages; i++ {
+		getPage(i)
+	}
 }
 
+func getPage(page int) {
+	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
+	fmt.Println("Requesting", pageURL)
+}
 func getPages() int {
+	pages := 0
 	res, err := http.Get(baseURL)
 	checkErr(err)
 	checkStatusCode(res)
+	defer res.Body.Close()
 	doc, err := goquery.NewDocumentFromReader(res.Body)
-	return 0
+	checkErr(err)
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length()
+	})
+	return pages
 }
 
 func checkErr(err error) {
