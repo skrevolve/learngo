540ef05fe98db6c95d81c2bc24493b1ce62e5e8d
skrevolve <sukyu0919@naver.com>
2021-05-07 18:16:29 +0900

    upload echo site

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
-	"net/http"
+	"strings"
 
 	"github.com/labstack/echo"
+	"github.com/seokyu/learngo/scrapper"
 )
 
 func handleHome(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello World")
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment("jobs.csv", "job.csv")
 }
 
 func main() {
 	e := echo.New()
 	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
 	e.Logger.Fatal(e.Start(":1323"))
 }
--- scrapper/scrapper.go
+++ scrapper/scrapper.go
@@ -61,10 +61,10 @@ func getPage(page int, url string, mainC chan<- []extractedJob) {
 
 func extractJob(card *goquery.Selection, c chan<- extractedJob) {
 	id, _ := card.Attr("data-jk")
-	title := cleanString(card.Find(".title>a").Text())
-	location := cleanString(card.Find(".sjcl").Text())
-	salary := cleanString(card.Find(".salaryText").Text())
-	summary := cleanString(card.Find(".summary").Text())
+	title := CleanString(card.Find(".title>a").Text())
+	location := CleanString(card.Find(".sjcl").Text())
+	salary := CleanString(card.Find(".salaryText").Text())
+	summary := CleanString(card.Find(".summary").Text())
 	c <- extractedJob{
 		id:       id,
 		title:    title,
@@ -74,10 +74,12 @@ func extractJob(card *goquery.Selection, c chan<- extractedJob) {
 
 }
 
-func cleanString(str string) string {
+// Clean String
+func CleanString(str string) string {
 	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
 
+// get Pages
 func getPages(url string) int {
 	pages := 0
 	res, err := http.Get(url)
