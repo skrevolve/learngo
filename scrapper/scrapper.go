2c273603715c428a6027278c7f650a25de3cc685
skrevolve <sukyu0919@naver.com>
2021-05-07 17:50:25 +0900

    scrapper modify

--- scrapper/scrapper.go
+++ scrapper/scrapper.go
@@ -25,9 +25,9 @@ func Scrape(term string) {
 	var baseURL string = "https://kr.indeed.com/jobs?q=" + term + "&limit=50"
 	var jobs []extractedJob
 	c := make(chan []extractedJob)
-	totalPages := getPages()
+	totalPages := getPages(baseURL)
 	for i := 0; i < totalPages; i++ {
-		go getPage(i, c)
+		go getPage(i, baseURL, c)
 	}
 	for i := 0; i < totalPages; i++ {
 		extractedJobs := <-c
@@ -37,10 +37,10 @@ func Scrape(term string) {
 	fmt.Println("Done, extracted", len(jobs))
 }
 
-func getPage(page int, mainC chan<- []extractedJob) {
+func getPage(page int, url string, mainC chan<- []extractedJob) {
 	var jobs []extractedJob
 	c := make(chan extractedJob)
-	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
+	pageURL := url + "&start=" + strconv.Itoa(page*50)
 	fmt.Println("Requesting", pageURL)
 	res, err := http.Get(pageURL)
 	checkErr(err)
@@ -78,9 +78,9 @@ func cleanString(str string) string {
 	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
 
-func getPages() int {
+func getPages(url string) int {
 	pages := 0
-	res, err := http.Get(baseURL)
+	res, err := http.Get(url)
 	checkErr(err)
 	checkStatusCode(res)
 	defer res.Body.Close()
